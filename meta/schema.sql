8cc48ad57d789062b363252ff7b38bd12cf5e699
Varun Reddy <varun.reddy.2673@gmail.com>
2024-04-16 00:39:45 +0530

    feat: procedure that deletes users and posts by tag

--- meta/schema.sql
+++ meta/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS "user_tags" (
 	"user_id" integer NOT NULL,
 	"tag_id" integer NOT NULL,
 	CONSTRAINT "user_tag_pk" PRIMARY KEY("user_id","tag_id"),
-  FOREIGN KEY ("user_id") REFERENCES "users"("id"),
+  FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
   FOREIGN KEY ("tag_id") REFERENCES "tag"("id")
 );
 
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS "post" (
 	"content" text NOT NULL,
   "duplicate_id" integer,
 	"created_at" timestamp DEFAULT now(),
-  FOREIGN KEY ("user_id") REFERENCES "users"("id"),
+  FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
   FOREIGN KEY ("duplicate_id") REFERENCES "post"("id")
 );
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS "post_tags" (
 	"post_id" integer NOT NULL,
 	"tag_id" integer NOT NULL,
 	CONSTRAINT "post_tag_pk" PRIMARY KEY("post_id","tag_id"),
-  FOREIGN KEY ("post_id") REFERENCES "post"("id"),
+  FOREIGN KEY ("post_id") REFERENCES "post"("id") ON DELETE CASCADE,
   FOREIGN KEY ("tag_id") REFERENCES "tag"("id")
 );
 
@@ -49,9 +49,9 @@ CREATE TABLE IF NOT EXISTS "comment" (
 	"parent_id" integer,
 	"content" text NOT NULL,
 	"created_at" timestamp DEFAULT now(),
-  FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-  FOREIGN KEY ("post_id") REFERENCES "post"("id"),
-  FOREIGN KEY ("parent_id") REFERENCES "comment"("id")
+  FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
+  FOREIGN KEY ("post_id") REFERENCES "post"("id") ON DELETE CASCADE,
+  FOREIGN KEY ("parent_id") REFERENCES "comment"("id") ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "sentiment" (
@@ -59,9 +59,21 @@ CREATE TABLE IF NOT EXISTS "sentiment" (
 	"comment_id" integer NOT NULL,
   "score" integer NOT NULL,
 	CONSTRAINT "user_comment_pk" PRIMARY KEY("user_id","comment_id"),
-  FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-  FOREIGN KEY ("comment_id") REFERENCES "comment"("id")
+  FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
+  FOREIGN KEY ("comment_id") REFERENCES "comment"("id") ON DELETE CASCADE
 );
+
+CREATE OR REPLACE PROCEDURE delete_by_tag(tag IN SERIAL) IS
+-- This procedure deletes all posts and users with a certain tag
+BEGIN
+	DELETE FROM users WHERE id IN (
+		SELECT user_id FROM user_tags WHERE tag_id=tag
+	);
+	DELETE FROM post WHERE id IN (
+		SELECT post_id FROM post_tags WHERE tag_id=tag
+	);
+	DELETE FROM tag WHERE id=tag;
+END;
 
 CREATE FUNCTION welcome_post()
 RETURN TRIGGER AS $$
